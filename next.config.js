8865d2b9bd09c5928193c9d63cea80a3f299b6d5
synteraai2025-collab <syntera.ai2025@gmail.com>
2026-02-15 23:27:24 +0200

    Add starter files for nextjs

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Important: Allow the dev server to be accessed from proxy
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  // Ensure dev server binds to 0.0.0.0
+  env: {
+    HOST: '0.0.0.0',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
